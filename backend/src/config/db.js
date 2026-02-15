e5cf8e77f4de6be20319e9225e973e697ac6e2ba
DinilDulneth <dinildulneth123@gmail.com>
2026-02-15 21:42:22 +0530

    changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default async function connectDB() {
   const MONGO_URI = process.env.MONGO_URI;
-  
+
   if (!MONGO_URI) {
-    throw new Error('MONGO_URI not set in environment');
+    throw new Error("MONGO_URI not set in environment");
   }
 
   await mongoose.connect(MONGO_URI);
   // eslint-disable-next-line no-console
-  console.log('MongoDB connected');
+  console.log("MongoDB connected");
 }
