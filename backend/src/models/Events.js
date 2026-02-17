0fd9d80c92bc42cb5ac82c1c1a81de6beedd150a
Shalon Fernando <shalonfernando52@gmail.com>
2026-02-18 00:16:18 +0530

    feat: add events model

---
+++ backend/src/models/Events.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema(
+  {
+    organizerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // The Mentor who created the event
+      required: true,
+    },
+    title: {
+      type: String,
+      required: [true, 'Please add an event title'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add a description or agenda'],
+    },
+    eventType: {
+      type: String,
+      enum: ['Webinar', 'Workshop', 'Pitch Day', 'Networking', 'Legal Session'],
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    link: { // Zoom or Google Meet link
+      type: String,
+      required: [true, 'Please add the meeting link'],
+    },
+    bannerImage: {
+      type: String, // URL to image (Cloudinary/S3)
+      default: 'default_event.jpg'
+    },
+    attendees: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User' // Investors/Startups who RSVP'd
+    }]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Event', eventSchema);
