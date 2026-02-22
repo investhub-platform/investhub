84fc5ba75123acf86206d8fe92e4fdbaee5968a3
DinilDulneth <dinildulneth123@gmail.com>
2026-02-22 19:17:40 +0530

    fix error

--- backend/src/models/BaseSchema.js
+++ backend/src/models/BaseSchema.js
@@ -1,15 +1,12 @@
 // models/BaseSchema.js
 import { Schema, Types } from "mongoose";
 
-export const BaseSchema = new Schema(
-  {
-    createdUtc: { type: Date, default: () => new Date() },
-    createdBy: { type: Types.ObjectId, ref: "User", required: true },
-    updatedUtc: { type: Date, default: () => new Date() },
-    updatedBy: { type: Types.ObjectId, ref: "User" },
-    deletedUtc: { type: Date, default: null },
-    deletedBy: { type: Types.ObjectId, ref: "User", default: null },
-    status: { type: String, default: "active" }
-  },
-  { _id: false } // don't want BaseSchema to create its own _id
-);
+export const BaseSchema = new Schema({
+  createdUtc: { type: Date, default: () => new Date() },
+  createdBy: { type: Types.ObjectId, ref: "User", required: true },
+  updatedUtc: { type: Date, default: null },
+  updatedBy: { type: Types.ObjectId, ref: "User", default: null },
+  deletedUtc: { type: Date, default: null },
+  deletedBy: { type: Types.ObjectId, ref: "User", default: null },
+  status: { type: String, default: "active" }
+});
