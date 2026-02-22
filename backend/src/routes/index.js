c16bd5ca7f24186b9e0699bded3d80d4b203377b
DinilDulneth <dinildulneth123@gmail.com>
2026-02-22 19:17:58 +0530

    fix error

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,9 @@
-import { Router } from 'express';
-import v1 from './v1/index.js';
+//src/route/index.js
+import { Router } from "express";
+import v1 from "./v1/index.js";
 
 const router = Router();
 
-router.use('/v1', v1);
+router.use("/v1", v1);
 
 export default router;
